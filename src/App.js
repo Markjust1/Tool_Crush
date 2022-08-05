91d5d900386798ecd03ec57d97ae4436e0356012
Markjust1 <novikov711@gmail.com>
2022-08-05 11:42:50 -0600

    add css and useeffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 const width = 8;
 const toolColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
-const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const checkForColumnOfThree = () => {
+    
+  };
 
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
-      const randomNumberFrom0To5 = Math.floor(Math.random() * toolColors.length);
+      const randomNumberFrom0To5 = Math.floor(
+        Math.random() * toolColors.length
+      );
       const randomColor = toolColors[randomNumberFrom0To5];
       randomColorArrangement.push(randomColor);
     }
+    setCurrentColorArrangement(randomColorArrangement);
   };
 
-  createBoard();
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  console.log(currentColorArrangement);
 
   return (
-    <div></div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((toolColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: toolColor }}
+            alt={toolColor}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
-
 
 export default App;
